97d6bc0882886b03aadcd858eaa67b89dcb5fa7d
Vineet Kumar <kvineet.java@gmail.com>
2021-01-25 21:14:16 +0530

    1st commit of Airport API

---
+++ airport-api-demo/src/main/java/com/vineet/airport/api/AirportApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vineet.airport.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportApiDemoApplication.class, args);
+	}
+
+}
---
+++ airport-api-demo/src/test/java/com/vineet/airport/api/AirportApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineet.airport.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
